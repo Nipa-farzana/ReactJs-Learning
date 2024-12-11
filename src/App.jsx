26ed9b0e8408e312cbc32b2d9eae79db75f92730
Nipa Akter <nipapstu2018@gmail.com>
2024-12-12 00:06:07 +0600

    component

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+export const App = () => {
+  return (
+<div>
+  <NetflixSeires />
+  <NetflixSeires />
+  <NetflixSeires />
+</div>
+  );
+};
+
+const NetflixSeires = () => {
+  return( <div>
+    <div>
+      <img src="images.jpeg" alt="images.jpeg" width= "30%" height= "60%" />
+    </div>
+  <h2> Name: Queen of Tears</h2>
+  <h3>Rating:7.8</h3>
+  <p>Summary:The queen of department stores and her small-town husband weather a marital crisis —until love miraculously begins to bloom again.</p>
+  </div>);
+
+};
+
+
+
